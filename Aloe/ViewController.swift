9f731a7efc44d2823870395732de614f57d304f7
jerin george <jerin.ejugiter@gmail.com>
2018-11-21 12:08:21 +0530

    Initial Commit

---
+++ Aloe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aloe
+//
+//  Created by Thomas chacko on 21/11/18.
+//  Copyright © 2018 Thomas chacko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
